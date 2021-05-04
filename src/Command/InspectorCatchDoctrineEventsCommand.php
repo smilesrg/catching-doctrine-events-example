8f7f9383211836a54da995ce10dc7a8e361c306c
Serhii Smirnov <free.smilesrg@gmail.com>
2021-05-04 17:33:38 +0300

    Added persisting an entity into command

--- src/Command/InspectorCatchDoctrineEventsCommand.php
+++ src/Command/InspectorCatchDoctrineEventsCommand.php
@@ -2,10 +2,10 @@
 
 namespace App\Command;
 
+use App\Entity\Phonebook;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -13,31 +13,40 @@ class InspectorCatchDoctrineEventsCommand extends Command
 {
     protected static $defaultName = 'inspector:catch-doctrine-events';
     protected static $defaultDescription = 'Add a short description for your command';
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        parent::__construct();
+    }
 
     protected function configure()
     {
         $this
             ->setDescription(self::$defaultDescription)
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $faker = \Faker\Factory::create();
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $phoneBook = new Phonebook();
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $phoneBook->setFirstName($faker->firstName());
+        $phoneBook->setLastName($faker->lastName());
+        $phoneBook->setPhoneNumber($faker->phoneNumber());
 
+        $this->entityManager->persist($phoneBook);
+        $this->entityManager->flush();
+
+        $io->success('Command finished successfully');
         return Command::SUCCESS;
     }
 }
