376fa3869227ee2e1c35eb508564f21800be7f1b
Serhii Smirnov <free.smilesrg@gmail.com>
2021-05-04 18:41:54 +0300

    Added Doctrine subscriber class

---
+++ src/EventListener/DatabaseActivitySubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+class DatabaseActivitySubscriber implements EventSubscriber
+{
+    // this method can only return the event names; you cannot define a
+    // custom method name to execute when each event triggers
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist,
+            Events::postRemove,
+            Events::postUpdate,
+        ];
+    }
+
+    // callback methods must be called exactly like the events they listen to;
+    // they receive an argument of type LifecycleEventArgs, which gives you access
+    // to both the entity object of the event and the entity manager itself
+    public function postPersist(LifecycleEventArgs $args): void
+    {
+        $this->logActivity('persist', $args);
+    }
+
+    public function postRemove(LifecycleEventArgs $args): void
+    {
+        $this->logActivity('remove', $args);
+    }
+
+    public function postUpdate(LifecycleEventArgs $args): void
+    {
+        $this->logActivity('update', $args);
+    }
+
+    private function logActivity(string $action, LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        // ... get the entity information and log it somehow
+    }
+}
